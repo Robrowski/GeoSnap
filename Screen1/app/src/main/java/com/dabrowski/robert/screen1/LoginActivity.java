d9846d8f818033bd9bf52867d552cd1ed40ac813
Robert Dabrowski <rpdabrowski@wpi.edu>
2015-03-21 17:47:17 -0400

    Hw1 - screen 1 completed

---
+++ Screen1/app/src/main/java/com/dabrowski/robert/screen1/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.dabrowski.robert.screen1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class LoginActivity extends Activity  {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
+
+
+
