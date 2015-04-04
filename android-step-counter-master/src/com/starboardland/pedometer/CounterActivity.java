88c87ae8bfd9e9ea523e2a2075822721d453e983
Robert Dabrowski <rpdabrowski@wpi.edu>
2015-04-03 22:54:03 -0400

    Got an API key and loaded a map

--- android-step-counter-master/src/com/starboardland/pedometer/CounterActivity.java
+++ android-step-counter-master/src/com/starboardland/pedometer/CounterActivity.java
@@ -1,13 +1,20 @@
 package com.starboardland.pedometer;
 
-import android.app.Activity;
 import android.content.Context;
-import android.hardware.*;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class CounterActivity extends Activity implements SensorEventListener {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class CounterActivity extends FragmentActivity implements SensorEventListener {
+    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
 
     private SensorManager sensorManager;
     private TextView count;
@@ -20,7 +27,9 @@ public class CounterActivity extends Activity implements SensorEventListener {
         count = (TextView) findViewById(R.id.count);
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        setUpMapIfNeeded();
     }
+
 
     @Override
     protected void onResume() {
@@ -32,6 +41,7 @@ public class CounterActivity extends Activity implements SensorEventListener {
         } else {
             Toast.makeText(this, "Count sensor not available!", Toast.LENGTH_LONG).show();
         }
+        setUpMapIfNeeded();
 
     }
 
@@ -54,4 +64,43 @@ public class CounterActivity extends Activity implements SensorEventListener {
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
     }
+
+    /**
+     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
+     * installed) and the map has not already been instantiated.. This will ensure that we only ever
+     * call {@link #setUpMap()} once when {@link #mMap} is not null.
+     * <p/>
+     * If it isn't installed {@link com.google.android.gms.maps.SupportMapFragment} (and
+     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
+     * install/update the Google Play services APK on their device.
+     * <p/>
+     * A user can return to this FragmentActivity after following the prompt and correctly
+     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
+     * have been completely destroyed during this process (it is likely that it would only be
+     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
+     * method in {@link #onResume()} to guarantee that it will be called.
+     */
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map_fragment))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    /**
+     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
+     * just add a marker near Africa.
+     * <p/>
+     * This should only be called once and when we are sure that {@link #mMap} is not null.
+     */
+    private void setUpMap() {
+//        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+    }
+
 }
