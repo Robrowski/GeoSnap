e67ccb0499e7563af1fa3899d29c3bf236a310a0
Robert Dabrowski <rpdabrowski@wpi.edu>
2015-04-04 20:46:50 -0400

    map jumps to current location
    There might be some extra implemented methods here.. not sure

--- android-step-counter-master/src/com/starboardland/pedometer/CounterActivity.java
+++ android-step-counter-master/src/com/starboardland/pedometer/CounterActivity.java
@@ -1,24 +1,31 @@
 package com.starboardland.pedometer;
 
+import android.app.Activity;
 import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Location;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
-public class CounterActivity extends FragmentActivity implements SensorEventListener {
+public class CounterActivity extends Activity implements SensorEventListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
-
+    GoogleApiClient mGoogleApiClient;
     private SensorManager sensorManager;
     private TextView count;
     boolean activityRunning;
+    private Location mLastLocation;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -28,6 +35,16 @@ public class CounterActivity extends FragmentActivity implements SensorEventList
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         setUpMapIfNeeded();
+
+        buildGoogleApiClient();
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
     }
 
 
@@ -42,7 +59,7 @@ public class CounterActivity extends FragmentActivity implements SensorEventList
             Toast.makeText(this, "Count sensor not available!", Toast.LENGTH_LONG).show();
         }
         setUpMapIfNeeded();
-
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(15));
     }
 
     @Override
@@ -84,7 +101,7 @@ public class CounterActivity extends FragmentActivity implements SensorEventList
         // Do a null check to confirm that we have not already instantiated the map.
         if (mMap == null) {
             // Try to obtain the map from the SupportMapFragment.
-            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map_fragment))
+            mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map_fragment))
                     .getMap();
             // Check if we were successful in obtaining the map.
             if (mMap != null) {
@@ -100,7 +117,39 @@ public class CounterActivity extends FragmentActivity implements SensorEventList
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
-//        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        mMap.setMyLocationEnabled(true);
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(15));
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        if (mLastLocation != null) {
+            LatLng ll = new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(ll));
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
 }
