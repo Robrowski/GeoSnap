a9d939780cff12a1bc4bb8f47b888485e1957ccf
mscosti <mscosti3@gmail.com>
2015-04-05 20:11:42 -0400

    refactored and made it so each segment is hidden until it is active

--- android-step-counter-master/src/com/starboardland/pedometer/CounterActivity.java
+++ android-step-counter-master/src/com/starboardland/pedometer/CounterActivity.java
@@ -28,25 +28,36 @@ import java.util.TimerTask;
 
 public class CounterActivity extends Activity implements SensorEventListener{
 
-    private SensorManager sensorManager;
-    private TextView currentSegmentValueTextView;
-    private TextView count;
+    // App state
     boolean activityRunning;
     boolean startLogging = false;
-    private Timer timer = new Timer();
     private int stepsSinceStartUp;
 
-    TimerTask timerTask;
+    // Gui state
     private int segment = 0;
+    private View currentSegmentView;
+    private TextView currentSegmentValueTextView;
 
-    StepDbHelper mDbHelper;
+    // Timer Related
+    private Timer timer = new Timer();
+    private TimerTask timerTask;
+    public static Handler mHandler;
 
-    public Handler mHandler;
+    // Database Helper
+    private StepDbHelper mDbHelper;
 
-    MapFragment mMapFragment;
-    GoogleMap googleMap;
+    // Location and Pedometer
+    private SensorManager sensorManager;
+    private MapFragment mMapFragment;
+    private GoogleMap googleMap;
 
-    private int[] segmentTextViewIds = {
+    // Constants
+    private final int MAKEVISIBLE = 1;
+    private final int NUMSEGMENTS = 8;
+    private final int SEGMENTDURATION = 60000; // in ms
+    private final int TIMERDELAY = 100; // in ms
+
+    private int[] SegmentViewIds = {
             R.id.segment1,
             R.id.segment2,
             R.id.segment3,
@@ -57,34 +68,6 @@ public class CounterActivity extends Activity implements SensorEventListener{
             R.id.segment8
     };
 
-    public class stepCountTimer extends TimerTask {
-        int ms = 0;
-        int initSteps;
-
-        public stepCountTimer(int firstSteps){
-            initSteps = firstSteps;
-        }
-
-        @Override
-        public void run() {
-            if (ms >= 5000){
-                ms = 0;
-                int finalSteps = stepsSinceStartUp - initSteps;
-                initSteps = stepsSinceStartUp;
-                finishSegment(finalSteps);
-            }
-            else{
-                ms += 100;
-                int segmentSteps = stepsSinceStartUp - initSteps;
-                Message m = new Message();
-                m.arg1 = segmentSteps;
-                mHandler.sendMessage(m);
-            }
-        }
-    }
-
-
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -92,8 +75,10 @@ public class CounterActivity extends Activity implements SensorEventListener{
         populateSegmentLabels();
 
         // get the first segment text view an mark it as current
-        View segmentView = findViewById(R.id.segment1);
-        currentSegmentValueTextView = (TextView) segmentView.findViewById(R.id.value);
+        currentSegmentView = findViewById(R.id.segment1);
+        currentSegmentView.setVisibility(View.VISIBLE);
+        currentSegmentValueTextView = (TextView) currentSegmentView.findViewById(R.id.value);
+
 
         // get the sensor manager
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
@@ -102,12 +87,15 @@ public class CounterActivity extends Activity implements SensorEventListener{
         // an update on the steps so far
         mHandler = new Handler() {
             public void handleMessage(Message msg) {
+                if (msg.arg2 == MAKEVISIBLE){
+                    currentSegmentView.setVisibility(View.VISIBLE);
+                }
                 currentSegmentValueTextView.setText(Integer.toString(msg.arg1));
             }
         };
 
         /**
-         * Initialize the map and have it update every ~20 seconds
+         * Initialize the map and have it update every ~1 second
          */
 
         mMapFragment = ((MapFragment)getFragmentManager().findFragmentById(R.id.map));
@@ -123,6 +111,7 @@ public class CounterActivity extends Activity implements SensorEventListener{
         // Getting the name of the best provider
         String provider = locationManager.getBestProvider(criteria, true);
 
+        // set our location to be updated every second
         locationManager.requestLocationUpdates(provider,1000,0, new LocationUpdater(googleMap));
 
 
@@ -141,14 +130,16 @@ public class CounterActivity extends Activity implements SensorEventListener{
     public void populateSegmentLabels(){
         // We have to manually set the labels for each Segment Text View Label since we
         // are using the same included xml layout for each segment
-        for (int i = 0; i < segmentTextViewIds.length; i++){
-            View segmentView = findViewById(segmentTextViewIds[i]);
+        for (int i = 0; i < SegmentViewIds.length; i++){
+            View segmentView = findViewById(SegmentViewIds[i]);
             TextView segmentTextView = (TextView) segmentView.findViewById(R.id.label);
             segmentTextView.setText("Segment " + Integer.toString(i+1) + " steps:");
         }
     }
 
     public void finishSegment(int segmentTotal){
+        Log.d("steps","current Segment: " + segment + " steps: " + segmentTotal);
+
         // Set the segment steps one last time to make sure it definitley
         // reflects what will be in the database
         Message m = new Message();
@@ -156,38 +147,66 @@ public class CounterActivity extends Activity implements SensorEventListener{
         mHandler.sendMessageAtFrontOfQueue(m);
 
         addToDatabase(segment,segmentTotal);
-        Log.v("steps","current Segment: " + segment + " steps: " + segmentTotal);
+
+        // We increment our segment, and stop if we have reached the maximum segment length
         segment++;
-        if (segment >= 8){
+        if (segment >= NUMSEGMENTS){
             stopCounting();
         }
         else{
-            // get the segment label_value view, which contains a value TextView that we update
-            View segmentView = findViewById(segmentTextViewIds[segment]);
+            // get the segment label_value view, which contains a value TextView that we now mark as
+            // current for proper updating
+            View segmentView = findViewById(SegmentViewIds[segment]);
+            currentSegmentView = segmentView;
             currentSegmentValueTextView = (TextView) segmentView.findViewById(R.id.value);
+
+            // Send a message to make the new segment visible, and initialized with 0 steps
+            Message visible = new Message();
+            visible.arg1 = 0;
+            visible.arg2 = MAKEVISIBLE;
+            mHandler.sendMessage(visible);
         }
 
     }
 
+    /**
+     * Kills the timer and totals all the steps taken
+     */
     public void stopCounting(){
+        // kill the timer, we are done step counting
         timer.cancel();
 
+        // get the database for reading, and query for all columns and rows
         SQLiteDatabase db = mDbHelper.getReadableDatabase();
         Cursor results = db.query(StepEntry.TABLE_NAME, null, null, null, null, null, null);
 
+        // calculate the total steps taken from all of the segments in the database
         int totalSteps = 0;
         while (results.moveToNext()) {
             int segmentTotal = results.getInt(results.getColumnIndex(StepEntry.COLUMN_SEGMENT_STEPS));
             totalSteps += segmentTotal;
         }
-        currentSegmentValueTextView = (TextView) findViewById(R.id.valueTotal);
+
+        // Get the total views so the handler can properly update them
+        currentSegmentView = findViewById(R.id.totalview);
+        currentSegmentValueTextView = (TextView) currentSegmentView.findViewById(R.id.valueTotal);
+
+
+        // Send the last message to the UI to make the Total visible
+        // and set the total in the text view
         Message m = new Message();
         m.arg1 = totalSteps;
+        m.arg2 = MAKEVISIBLE;
         mHandler.sendMessageAtFrontOfQueue(m);
 
-        Log.v("stepper", "total steps: " + totalSteps);
+        Log.d("stepper", "total steps: " + totalSteps);
     }
 
+    /**
+     * Adds the total for the given segment's steps to the sqLite database
+     * @param segment
+     * @param segmentTotal
+     */
     public void addToDatabase(int segment,int segmentTotal){
         // Gets the data repository in write mode
         SQLiteDatabase db = mDbHelper.getWritableDatabase();
@@ -197,16 +216,13 @@ public class CounterActivity extends Activity implements SensorEventListener{
         values.put(StepEntry.COLUMN_SEGMENT, segment);
         values.put(StepEntry.COLUMN_SEGMENT_STEPS, segmentTotal);
 
-        // Insert the new row, returning the primary key value of the new row
-        long newRowId;
-        newRowId = db.insert(
-                StepEntry.TABLE_NAME,
-                null,
-                values);
-
+        // Insert the new row
+        db.insert(StepEntry.TABLE_NAME, null, values);
     }
 
-
+    /**
+     * Set the step counter to be active again on Resume
+     */
     @Override
     protected void onResume() {
         super.onResume();
@@ -220,6 +236,9 @@ public class CounterActivity extends Activity implements SensorEventListener{
 
     }
 
+    /**
+     * mark that our app is not currently running so our app has time to react accordingly
+     */
     @Override
     protected void onPause() {
         super.onPause();
@@ -228,16 +247,26 @@ public class CounterActivity extends Activity implements SensorEventListener{
 //        sensorManager.unregisterListener(this); 
     }
 
+    /**
+     * Called whenever the pedometer has a new total step value, it stores the new step
+     * value for later use.  We also start the timer here on first sensor reading.  We wait until
+     * now to start the timer so we know and have an initial stepscount to compare against.
+     * @param event
+     */
     @Override
     public void onSensorChanged(SensorEvent event) {
         if (activityRunning) {
+            // If this is our first sensor reading, we can start the timertask to begin
             if (startLogging == false){
                 startLogging = true;
+                // initialize the timer task with the initial steps count
                 timerTask = new stepCountTimer((int) event.values[0]);
-                timer.schedule(timerTask, 0, 100);
-                Log.v("sensorchanged","startlogging");
+                // run the timer immediatley every 100ms
+                timer.schedule(timerTask, 0, TIMERDELAY);
+                Log.d("sensorchanged","startlogging");
 
             }
+            // reset our count to be the new total steps from our sensor
             stepsSinceStartUp = (int) event.values[0];
         }
 
@@ -245,5 +274,31 @@ public class CounterActivity extends Activity implements SensorEventListener{
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    public class stepCountTimer extends TimerTask {
+        int ms = 0;
+        int initSteps;
+
+        public stepCountTimer(int firstSteps){
+            initSteps = firstSteps;
+        }
+
+        @Override
+        public void run() {
+            if (ms >= SEGMENTDURATION){
+                ms = 0;
+                int finalSteps = stepsSinceStartUp - initSteps;
+                initSteps = stepsSinceStartUp;
+                finishSegment(finalSteps);
+            }
+            else{
+                ms += TIMERDELAY;
+                int segmentSteps = stepsSinceStartUp - initSteps;
+                Message m = new Message();
+                m.arg1 = segmentSteps;
+                mHandler.sendMessage(m);
+            }
+        }
     }
 }
