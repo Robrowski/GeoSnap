0af92e5a0d3b7c448a6ac2fd570fa92487f09039
mscosti <mscosti3@gmail.com>
2015-04-07 19:30:55 -0400

    added geocloud, our app engine backend

---
+++ GeoSnap/GeoCloud/src/main/java/edu/cs430x/fuschia/geocloud/MyBean.java
@@ -0,0 +1,17 @@
+package edu.cs430x.fuschia.geocloud;
+
+/**
+ * The object model for the data we are sending through endpoints
+ */
+public class MyBean {
+
+    private String myData;
+
+    public String getData() {
+        return myData;
+    }
+
+    public void setData(String data) {
+        myData = data;
+    }
+}
