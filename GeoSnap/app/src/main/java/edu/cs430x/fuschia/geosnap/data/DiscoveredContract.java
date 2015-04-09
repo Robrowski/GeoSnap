b4040e5a41f844616365b7d59110dd3fe3e5ba63
mscosti <mscosti3@gmail.com>
2015-04-09 19:02:30 -0400

    added local discovered database contract and helper

---
+++ GeoSnap/app/src/main/java/edu/cs430x/fuschia/geosnap/data/DiscoveredContract.java
@@ -0,0 +1,21 @@
+package edu.cs430x.fuschia.geosnap.data;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by Matt on 4/9/2015.
+ * <p/>
+ * Defines table and column names for the weather database.
+ */
+public class DiscoveredContract {
+
+    /* Inner class that defines the table contents */
+    public static abstract class DiscoveredEntry implements BaseColumns {
+        public static final String TABLE_NAME = "discovered";
+        public static final String COLUMN_PHOTO_LOC_ = "photo_loc";
+        public static final String COLUMN_COORD_LAT = "latitude";
+        public static final String COLUMN_COORD_LON = "longitude";
+        public static final String COLUMN_DISCOVER = "discoverability";
+        public static final String COLUMN_TIMESTAMP = "time_stamp";
+    }
+}
---
+++ GeoSnap/app/src/main/java/edu/cs430x/fuschia/geosnap/data/DiscoveredSnapsDBHelper.java
@@ -0,0 +1,44 @@
+package edu.cs430x.fuschia.geosnap.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import edu.cs430x.fuschia.geosnap.data.DiscoveredContract.DiscoveredEntry;
+
+/**
+ * Created by Matt on 4/9/2015.
+ */
+public class DiscoveredSnapsDBHelper extends SQLiteOpenHelper {
+    // If you change the database schema, you must increment the database version.
+    private static final int DATABASE_VERSION = 1;
+
+    static final String DATABASE_NAME = "discovered.db";
+
+    public DiscoveredSnapsDBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+}
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        final String SQL_CREATE_DISCOVERED_TABLE = "CREATE TABLE " + DiscoveredEntry.TABLE_NAME + " (" +
+
+                // ID will be assigned by the server
+                DiscoveredEntry._ID + " INTEGER PRIMARY KEY," +
+
+                // the ID of the location entry associated with this weather data
+                DiscoveredEntry.COLUMN_PHOTO_LOC_ + " TEXT NOT NULL, " +
+                DiscoveredEntry.COLUMN_COORD_LAT + " REAL NOT NULL, " +
+                DiscoveredEntry.COLUMN_COORD_LON + " REAL NOT NULL, " +
+                DiscoveredEntry.COLUMN_DISCOVER + " INTEGER NOT NULL," +
+
+                // Have to agree on timestamp format. Assuming UTC formatted String
+                DiscoveredEntry.COLUMN_TIMESTAMP + " TEXT NOT NULL); ";
+
+        sqLiteDatabase.execSQL(SQL_CREATE_DISCOVERED_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i2) {
+
+    }
+}
