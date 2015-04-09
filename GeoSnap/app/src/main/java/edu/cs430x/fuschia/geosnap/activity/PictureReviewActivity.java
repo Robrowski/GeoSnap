9984ac3576588e6ed80d346a1e20d46ff6bdc4ec
Robert Dabrowski <rpdabrowski@wpi.edu>
2015-04-08 23:53:04 -0400

    set up picture review activity
    This activity is for reviewing a picture before sending it to the server

---
+++ GeoSnap/app/src/main/java/edu/cs430x/fuschia/geosnap/activity/PictureReviewActivity.java
@@ -0,0 +1,61 @@
+package edu.cs430x.fuschia.geosnap.activity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import edu.cs430x.fuschia.geosnap.R;
+
+
+/** This is written as an activity because the amount of interaction between the fragment and activity was way stupid */
+public class PictureReviewActivity extends ActionBarActivity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_picture_review);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_picture_preview, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public void onAccept(Uri u) {
+        // Send picture to server, etc.
+
+        // Return to picture view
+
+        // Toast
+    }
+
+    /** This also needs to happen when the back button is pressed */
+    public void onReject(Uri u) {
+    // Trash the image
+    // return to picture view
+
+    // Toast file deleted
+
+    }
+}
