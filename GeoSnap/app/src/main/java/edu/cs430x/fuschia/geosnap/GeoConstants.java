95d860e87dff2b3edce14455b10ad24e7096200d
Robert Dabrowski <rpdabrowski@wpi.edu>
2015-04-30 00:01:54 -0400

    updated location service settings

--- GeoSnap/app/src/main/java/edu/cs430x/fuschia/geosnap/GeoConstants.java
+++ GeoSnap/app/src/main/java/edu/cs430x/fuschia/geosnap/GeoConstants.java
@@ -1,5 +1,7 @@
 package edu.cs430x.fuschia.geosnap;
 
+import com.google.android.gms.location.LocationRequest;
+
 /**
  * Created by Robrowski on 4/28/2015.
  */
@@ -33,12 +35,11 @@ public interface GeoConstants {
 
 
     public interface LocationDefaults{
-        public static final String REQUEST_PRIORITY = "0", /* balanced power */
-                LOCATION_INTERVAL = "3", /* minutes between official GPS location requests  */
+        public static final String ACTIVITY_INTERVAL = "30",/* seconds between activity updates*/
+                LOCATION_INTERVAL = "1", /* minutes between official GPS location requests  */
                 FASTEST_INTERVAL = "15", /* seconds between accepting location updates */
-                SMALLEST_DISPLACEMENT = "0.0", /* meters */
-                ACTIVITY_INTERVAL = "30"; /* seconds between activity updates*/
-
+                SMALLEST_DISPLACEMENT = "1.0", /* meters */
+                REQUEST_PRIORITY = String.valueOf(LocationRequest.PRIORITY_HIGH_ACCURACY);
 
     }
 
